d85feff41785053ac3f254e51e64bdbea7ccbae2
yury.gubkin <yury.gubkin@streamlabs.ru>
2024-09-10 17:40:10 +0300

    add: переделал на сканирование файла

--- src/number-words-text.c
+++ src/number-words-text.c
@@ -4,64 +4,102 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define stdin in
+
 typedef struct {
     void *next_element;
-    char word[];
+    int word_start_index;
  }word_list;
 
-word_list *insert_new_word(word_list *words, char *new_word)
+static bool is_separator(char sym)
 {
-    word_list *new_element = malloc(sizeof(word_list) + strlen(new_word) + sizeof('\0'));
-    strcpy(new_element->word, new_word);
-    new_element->next_element = words;
-    return new_element;
+    return sym == ' ' || sym == '\n';
 }
 
-void print_words(word_list *word)
+static bool is_equal_word(int word_index1, int word_index2)
 {
-    while (word != NULL)
-    {
-        printf("%s\n", word->word);
-        word = word->next_element;
-    }
-}
-
-bool is_already_in_array(word_list *words, char new_word[])
-{
-    while (words != NULL)
-    {
-        if(strcmp(words->word, new_word) == 0)
+    char sym1, sym2;
+    do {
+        fseek(stdin, word_index1, SEEK_SET);
+        sym1 = fgetc(stdin);
+        fseek(stdin, word_index2, SEEK_SET);
+        sym2 = fgetc(stdin);
+        if (is_separator(sym1) && is_separator(sym2))
         {
             return true;
         }
-        words = words->next_element;
-    }
+
+        word_index1++;
+        word_index2++;
+    } while(sym1 == sym2);
+
     return false;
+}
+
+static word_list *insert_new_word(word_list *first_word, int new_word_index)
+{
+    word_list *new_element = malloc(sizeof(word_list));
+    new_element->word_start_index = new_word_index;
+    if (first_word != NULL)
+    {
+        new_element->next_element = first_word;
+    }
+    else
+    {
+        new_element->next_element = NULL;
+    }
+    return new_element;
+}
+
+static int length(word_list *first_word)
+{
+    int length = 0;
+    word_list *word = first_word;
+    while(word != NULL)
+    {
+        char buf[100];
+        fseek(stdin, word->word_start_index, SEEK_SET);
+        fscanf(stdin, "%s", buf);
+        length++;
+        word = word->next_element;
+    }
+    return length;
+}
+
+static word_list *add_unique_word(word_list *first_word, int new_word_index)
+{
+    word_list *word = first_word;
+    while(word != NULL)
+    {
+        if(is_equal_word(word->word_start_index, new_word_index))
+        {
+            return first_word;
+        }
+        word = word->next_element;
+    }
+    return insert_new_word(first_word, new_word_index);
 }
 
 int main()
 {
-    char word_buf[0xffff];
-    if (scanf("%s", word_buf) != 1)
-    {
-        printf("%d", 0);
-        return 0;
-    }
     word_list *first_word = NULL;
-    first_word = malloc(sizeof(word_list) + strlen(word_buf) + sizeof('\0'));
-    strcpy(first_word->word, word_buf);
-    first_word->next_element = NULL;
-    int word_count = 1;
-    
-    while(scanf("%s", word_buf) == 1)
+    char sym = fgetc(stdin);
+    int index = 0;
+    bool last_sym_separator = true;
+    while (sym != EOF)
     {
-        sizeof(word_list);
-        if(!is_already_in_array(first_word, word_buf))
+        if(!is_separator(sym) && last_sym_separator)
         {
-            first_word = insert_new_word(first_word, word_buf);
-            word_count++;
+            fpos_t position;
+            fgetpos(stdin, &position);
+            first_word = add_unique_word(first_word, index);
+            fsetpos(stdin, &position);
         }
+        last_sym_separator = is_separator(sym);
+        index++;
+        fseek(stdin, index, SEEK_SET);
+        sym = fgetc(stdin);
     }
-    printf("%d", word_count);
+    printf("%d", length(first_word));
     return 0;
 }
